10e1b1c821cea7ecf6b8f664f3d4b91e970451ae
Carson Full <carson.full@gmomail.org>
2015-05-04 13:05:34 -0500

    Fixed Verify Tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,15 @@
 
 namespace Nexmo\Tests;
 
+use GuzzleHttp as Guzzle;
+
 class TestCase extends \PHPUnit_Framework_TestCase
 {
-
+    /**
+     * @return Guzzle\Client|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function guzzle()
+    {
+        return $this->getMock('\GuzzleHttp\ClientInterface');
+    }
 }
