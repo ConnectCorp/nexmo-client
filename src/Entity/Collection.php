713cdb38fd0725efec9372fdfe1916db120e6486
Carson Full <carson.full@gmomail.org>
2015-05-05 09:19:38 -0500

    Added DocBlocks to Collection

--- src/Entity/Collection.php
+++ src/Entity/Collection.php
@@ -7,10 +7,19 @@ use ArrayIterator;
 use Countable;
 use IteratorAggregate;
 
+/**
+ * An Immutable key value pair collection
+ */
 class Collection implements ArrayAccess, Countable, IteratorAggregate
 {
-    protected $data = [];
+    /**
+     * @var array
+     */
+    protected $data;
 
+    /**
+     * @param array|Collection $data
+     */
     public function __construct($data)
     {
         if ($data instanceof Collection) {
@@ -20,7 +29,10 @@ class Collection implements ArrayAccess, Countable, IteratorAggregate
     }
 
     /**
+     * Get an item from collection with the given key.
+     *
      * @param string $key
+     *
      * @return mixed|null
      */
     public function get($key)
@@ -29,8 +41,12 @@ class Collection implements ArrayAccess, Countable, IteratorAggregate
     }
 
     /**
+     * Get a list item from collection with given key.
+     * If item does not exist an empty array is returned.
+     *
      * @param string        $key
-     * @param callable|null $callback
+     * @param callable|null $callback Optional callable to map items in list
+     *
      * @return array
      */
     public function getArray($key, callable $callback = null)
@@ -45,39 +61,69 @@ class Collection implements ArrayAccess, Countable, IteratorAggregate
         return $list;
     }
 
+    /**
+     * Return whether collection has an item with the given key.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
     public function has($key)
     {
         return array_key_exists($key, $this->data);
     }
 
+    /**
+     * Return all the items in collection.
+     *
+     * @return array
+     */
     public function all()
     {
         return $this->data;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function offsetExists($offset)
     {
         return $this->has($offset);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function offsetGet($offset)
     {
         return $this->get($offset);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function offsetSet($offset, $value)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function offsetUnset($offset)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getIterator()
     {
         return new ArrayIterator($this->data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function count()
     {
         return count($this->data);
